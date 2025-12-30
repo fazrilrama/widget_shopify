55ad3c7159eedfcf3a177c02784600332c0db2cd
lutfiadysh <46888540+lutfiadysh@users.noreply.github.com>
2025-12-30 16:39:41 +0700

    Update widget.js

--- widget.js
+++ widget.js
@@ -521,14 +521,16 @@ if (typeof memloadedPlanWidgetJS === 'undefined') {
                                 });
                             } else {
 								let name_custom = '';
-								console.log($jQNoConflict(this).attr('data-name'));
-								if($jQNoConflict(this).attr('data-name') == '¥500 Monthly') {
-									console.log('kesini 1');
-									name_custom = '月額500円';
+								let raw = ($jQNoConflict(this).attr('data-name') + '').trim();
+								let clean = raw.replace(/[¥,]/g, '');
+								
+								console.log(raw);
+								
+								if (clean === '500 Monthly') {
+								    name_custom = '月額500円';
 								}
-								if($jQNoConflict(this).attr('data-name') == '¥5,000 Yearly') {
-									console.log('ini 2'):
-									name_custom = '年額5,000円';
+								if (clean === '5000 Yearly') {
+								    name_custom = '年額5,000円';
 								}
 								console.log($jQNoConflict('#' + plantag + '_membership_billing_option option:selected').text());
                                 stripeHandler.open({
